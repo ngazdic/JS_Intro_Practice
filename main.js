d0cbabbc864ca84f86eb25e30cfbdabd94e4304a
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-08-08 13:16:26 -0400

    Done

---
+++ main.js
@@ -0,0 +1,28 @@
+const firstName = 'Nikola';
+let lastName = 'Gazdic';
+
+let age = 31;
+
+let exercise = document.getElementById('Exercise');
+exercise.innerHTML = firstName + ' ' + lastName + ' ' + age;
+
+let language = 'JavaScript';
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let exercise1 = document.getElementById('Exercise1');
+exercise1.innerHTML = language + ' ' + createdYear + ' ' + isCaseSensitive;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+
+let stock = 0;
+let inStock = false;
+let selectedSize = 'M';
+
+let title = 'Name of the Wind';
+let author = 'Patrick Rothfuss';
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
